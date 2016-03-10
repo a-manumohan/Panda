8bddc1b39a7ed725bb875454c12d22ad3d6e5ff4
manuMohan <manu@gramble.com>
2016-03-10 15:10:46 +0100

    Initial commit

---
+++ app/src/main/java/in/co/mn/panda/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package in.co.mn.panda.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.co.mn.panda.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
